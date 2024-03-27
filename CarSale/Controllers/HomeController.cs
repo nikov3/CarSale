d9117fbb669c4bcc355441ff492a23966f27cf8a
Nikolay Nikov <nikovn557@gmail.com>
2024-03-28 00:02:24 +0200

    Layout tweaked

--- CarSale/Controllers/HomeController.cs
+++ CarSale/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace CarSale.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
