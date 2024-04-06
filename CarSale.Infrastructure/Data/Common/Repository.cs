5fb94e54332959b434e0e56ead760a402499bbf5
Nikolay Nikov <nikovn557@gmail.com>
2024-04-06 17:43:09 +0300

    Repository added

---
+++ CarSale.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarSale.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(CarSaleDbContext _context)
+        {
+            context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        } 
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+    }
+}
