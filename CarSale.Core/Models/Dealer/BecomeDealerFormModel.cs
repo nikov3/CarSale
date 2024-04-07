8abf56ae2ef78ca2569ded1811ac22e6de63ed60
Nikolay Nikov <nikovn557@gmail.com>
2024-04-07 17:54:15 +0300

    BecomeDealerFormModel created

--- CarSale.Core/Models/Dealer/BecomeDealerFormModel.cs
+++ CarSale.Core/Models/Dealer/BecomeDealerFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace CarSale.Core.Models.Dealer
+﻿using System.ComponentModel.DataAnnotations;
+using static CarSale.Core.Constants.MessageConstants;
+using static CarSale.Infrastructure.Constants.DataConstants.Dealer;
+
+namespace CarSale.Core.Models.Dealer
 {
     public class BecomeDealerFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PhoneMaxLength, 
+            MinimumLength = PhoneMinLength,
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
     }
 }
