cd28592f908cb879c43c48b83e5fcd4c6f3a5ce4
Nikolay Nikov <nikovn557@gmail.com>
2024-04-07 18:35:13 +0300

    NotADealerAttribute created

---
+++ CarSale/Attributes/NotADealerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using CarSale.Core.Contracts;
+using CarSale.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CarSale.Attributes
+{
+    public class NotADealerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IDealerService? dealerService = context.HttpContext.RequestServices.GetService<IDealerService>();
+
+            if (dealerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (dealerService != null
+                && dealerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
