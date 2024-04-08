73b623097cb1da89541c8bbf35d7c49cac28ceee
Nikolay Nikov <nikovn557@gmail.com>
2024-04-08 17:39:30 +0300

    Added constants

--- CarSale.Infrastructure/Constants/DataConstants.cs
+++ CarSale.Infrastructure/Constants/DataConstants.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,16 +20,19 @@ namespace CarSale.Infrastructure.Constants
             public const int NameMaxLength = 50;
             public const int NameMinLength = 2;
         }
+
         public static class City
         {
             public const int NameMaxLength = 50;
             public const int NameMinLength = 2;
         }
+        
         public static class Color
         {
             public const int NameMaxLength = 50;
             public const int NameMinLength = 2;
         }
+        
         public static class Dealer
         {
             public const int NameMaxLength = 50;
@@ -46,16 +50,28 @@ namespace CarSale.Infrastructure.Constants
             public const int NameMaxLength = 20;
             public const int NameMinLength = 2;
         }
+        
         public static class Model
         {
             public const int NameMaxLength = 30;
             public const int NameMinLength = 2;
         }
-        //public static class Offer
-        //{
-        //    public const int MilageMaxLength = 50;
-        //    public const int MilageMinLength = 2;
-        //}
+        
+        public static class Offer
+        {
+            public const int DescriptionMaxLength = 200;
+            public const int DescriptionMinLength = 5;
+
+            public const string PriceMaximum = "1000000";
+            public const string PriceMinimum = "0";
+
+            public const string YearMaximum = "2024";
+            public const string YearMinimum = "1950";
+
+            public const string MilageMaximum = "1000000";
+            public const string MilageMinimum = "0";
+        }
+        
         public static class Transmission
         {
             public const int NameMaxLength = 20;
