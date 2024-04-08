c6aeb0e9e423939a0e022c4dcbd2c36948728611
Nikolay Nikov <nikovn557@gmail.com>
2024-04-08 17:35:56 +0300

    Added atrtribute MustBeADealer

---
+++ CarSale/Attributes/MustBeADealer.cs
@@ -0,0 +1,29 @@
+﻿using CarSale.Core.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using CarSale.Extensions;
+using CarSale.Controllers;
+
+namespace CarSale.Attributes
+{
+    public class MustBeADealer : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IDealerService? dealerService = context.HttpContext.RequestServices.GetService<IDealerService>();
+
+            if (dealerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (dealerService != null
+                && dealerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(DealerController), "Dealer", null);
+            }
+        }
+    }
+}
