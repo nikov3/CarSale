4471b906ba069b634508f31f056d35431c10ce47
Nikolay Nikov <nikovn557@gmail.com>
2024-04-12 21:45:26 +0300

    Created OfferSorting enum

---
+++ CarSale.Core/Enumerations/OfferSorting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSale.Core.Enumerations
+{
+    public enum OfferSorting
+    {
+        Newest = 0,
+        Price = 1,
+        CarYear = 2
+    }
+}
